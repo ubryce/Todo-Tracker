9851defa2cda23288b1cd7696828043b1079d2c3
ubryce <bknguyen@ualberta.ca>
2020-07-27 16:57:13 -0600

    todo schema

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 app.use(cookieParser());
 app.use(express.json());
 
@@ -13,20 +14,21 @@ mongoose.connect('mongodb+srv://bryce:1234@cluster0-wbncb.mongodb.net/trades?ret
     console.log('successfully connected to database');
 });
 
-const User = require('./models/User');
-
+// EXAMPLE ADDING NEW USER 
+/*
 const userInput = {
     username : "bryce1234",
     password : "1234",
     role : "admin"
 }
-
 const user = new User(userInput);
 user.save((err,document)=>{
     if(err)
         console.log(err);
     console.log(document);
 })
+*/
+//ENDS HERE-------------------------
 
 app.listen(5000, ()=>{
     console.log('express server started on port 5000');
--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-
 const UserSchema = new mongoose.Schema({
     username : {
         type: String,
