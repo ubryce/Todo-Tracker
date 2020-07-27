a848fa94fea12c108dfb89b37cdc582e9f2e8911
ubryce <bknguyen@ualberta.ca>
2020-07-27 16:50:35 -0600

    userSchema + methods and first user created

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ mongoose.connect('mongodb+srv://bryce:1234@cluster0-wbncb.mongodb.net/trades?ret
     console.log('successfully connected to database');
 });
 
+const User = require('./models/User');
+
+const userInput = {
+    username : "bryce1234",
+    password : "1234",
+    role : "admin"
+}
+
+const user = new User(userInput);
+user.save((err,document)=>{
+    if(err)
+        console.log(err);
+    console.log(document);
+})
+
 app.listen(5000, ()=>{
     console.log('express server started on port 5000');
 });
--- models/User.js
+++ models/User.js
@@ -20,3 +20,31 @@ const UserSchema = new mongoose.Schema({
     },
     todos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Todo'}]
 });
+
+//check if password is hashed
+UserSchema.pre('save',function(next){
+    if(!this.isModified('password'))
+        return next();
+    bcrypt.hash(this.password,10,(err,passwordHash)=>{
+        if(err)
+            return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+
+//compare passwords
+UserSchema.methods.comparePassword = function(password,cb){
+    bcrypt.compare(password,this.password,(err,isMatch)=>{
+        if(err)
+            return cb(err);
+        else{
+            if(!isMatch)
+                return cb(null,isMatch);
+            //attach user object to request object
+            return cb(null,this);
+        }
+    });
+}
+
+module.exports = mongoose.model('User',UserSchema);
