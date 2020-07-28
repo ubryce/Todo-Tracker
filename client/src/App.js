d1faaa4f0d4521ae5f561ae669253bb21ded41ce
ubryce <bknguyen@ualberta.ca>
2020-07-28 15:54:27 -0600

    login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from "./Components/Home";
+import Login from "./Components/Login";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Navbar/>
       <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
     </Router>
   );
 }
